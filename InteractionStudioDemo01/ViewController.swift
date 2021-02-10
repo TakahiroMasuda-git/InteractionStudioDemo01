b6703e5566ced0e3230e21c16ad142fbe809e2ae
Takahiro Masuda <takahiro.masuda@salesforce.com>
2021-02-10 23:32:21 +0900

    Initial Commit

---
+++ InteractionStudioDemo01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InteractionStudioDemo01
+//
+//  Created by Takahiro Masuda on 2021/02/10.
+//  Copyright © 2021 salesforce.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InteractionStudioDemo01Tests/InteractionStudioDemo01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  InteractionStudioDemo01Tests.swift
+//  InteractionStudioDemo01Tests
+//
+//  Created by Takahiro Masuda on 2021/02/10.
+//  Copyright © 2021 salesforce.com. All rights reserved.
+//
+
+import XCTest
+@testable import InteractionStudioDemo01
+
+class InteractionStudioDemo01Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
